2d88fc1048fd4941211e6b5af57509b3077f2a41
Johan Gonzalez <jopi20101@gmail.com>
2022-10-23 13:44:00 -0500

    Done: encrypter and decrypter

---
+++ decryptor/src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        Decryptor
+    </h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+
+            <input type="file" name="file" />
+
+        <br><br>
+        <input type="file" name="key" placeholder="Select the private key" />
+        <br><br>
+        <button>Load file</button>
+    </form>
+</body>
+
+</html>
---
+++ decryptor/src/upload.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "./encryption.php";
+    require_once "./file_handler.php";
+
+    $encrypted_text = read_file(upload_file('file','files',false));
+
+    $key = read_file(upload_file('key','files',false));
+
+    $result = decrypt($encrypted_text,$key);
+    echo "<h2>The file contents:</h2>";
+    echo "<br>";
+    echo $result;
+?>
